aeb7d28df52cea40715660678ee8cf6e1bf31410
IamCJW <1990922775@qq.com>
2018-09-06 15:23:38 +0800

    节点组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <timeTag/>
+    <timeTag :tag-list="tagList"/>
   </div>
 </template>
 
 <script>
-import timeTag from './components/timeTag'
+  import timeTag from './components/timeTag'
 
-export default {
-  name: 'App',
-  components: {
-    timeTag
+  export default {
+    name: 'App',
+    components: {
+      timeTag
+    },
+    data(){
+      return{
+        tagList:[{
+          title:'发布招标日期',
+          time:'17/11/08'
+        },{
+          title:'招标日期',
+          time:'17/11/08'
+        },{
+          title:'发布日期',
+          time:'17/11/08'
+        },{
+          title:'发标日期',
+          time:''
+        },{
+          title:'布招日期',
+          time:''
+        }]
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/timeTag.vue
+++ src/components/timeTag.vue
@@ -1,113 +1,108 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="tag-container">
+    <div class="tag-group">
+      <div class="tag-item" v-for="(item,index) in tagList" :class="{'active':item.time}">
+        <div class="tag-title">{{item.title}}</div>
+        <div class="tag-up">上节点</div>
+        <div class="tag-link">中间节点</div>
+        <div class="tag-down">下节点</div>
+        <div class="tag-downLink"></div>
+        <div class="tag-time">{{item.time}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'timeTag',
+  props:{
+    tagList:{}
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="stylus">
+$main-color = #04a3ee
+$inactivation-color = #ddd
+.tag-container
+  background #fff
+  box-shadow 0 0 10px $inactivation-color
+  .tag-group
+    display flex
+    flex-wrap wrap
+    justify-content space-around
+    .tag-item
+      width 20%
+      display flex
+      flex-direction column
+      align-items center
+      .tag-title,.tag-time
+        font-size 12px
+        white-space nowrap
+        width 100%
+        overflow hidden
+        text-overflow ellipsis
+      .tag-link
+        font-size 0
+        height 20px
+        width 0
+        border-left 1px dashed rgba(221, 221, 221, 0.5)
+      &.active
+        .tag-link,.tag-downLink:before,.tag-downLink:after
+          border-color rgba(4, 163, 238, 0.5)
+        .tag-up
+          background $main-color
+        .tag-down
+          background rgba(4, 163, 238, 0.1)
+          &:after
+            background $main-color
+      .tag-downLink
+        position relative
+        width 100%
+        height 1px
+        &:after,&:before
+          content: ''
+          height 1px
+          position absolute
+          border-top 1px dashed rgba(221, 221, 221, 0.5)
+          bottom 14px
+        &:before
+          left 0
+          right 50%
+        &:after
+          left 50%
+          right 0
+      .tag-up,.tag-down
+        color transparent
+        border-radius 100%
+        font-size 0
+      .tag-up
+        margin-top 5px
+        width 5px
+        height 5px
+        background $inactivation-color
+      .tag-down
+        $cell = 3px
+        position relative
+        top -6px
+        width 15px
+        height 15px
+        background rgba(221, 221, 221, 0.19)
+        z-index 2
+        &:after
+          content: ''
+          position absolute
+          left $cell
+          right $cell
+          top $cell
+          bottom $cell
+          background $inactivation-color
+          border-radius 100%
 </style>
